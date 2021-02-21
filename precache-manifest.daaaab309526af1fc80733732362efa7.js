6e282e4c08e79dd26106244d28cfc1458f0a009a
robsonmanata <70637849+robsonmanata@users.noreply.github.com>
2021-02-21 11:46:26 +0100

    Updates

---
+++ precache-manifest.daaaab309526af1fc80733732362efa7.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0528f750d980f9e8670cfd6020366b57",
+    "url": "/to-do-list/index.html"
+  },
+  {
+    "revision": "e5f2aec77137107e26d1",
+    "url": "/to-do-list/static/js/2.c475bd67.chunk.js"
+  },
+  {
+    "revision": "a6a3dbc9bfefd7f42a14",
+    "url": "/to-do-list/static/js/main.a302910e.chunk.js"
+  },
+  {
+    "revision": "c85a1aa75588f403ca1f",
+    "url": "/to-do-list/static/js/runtime-main.1be551e3.js"
+  }
+]);
